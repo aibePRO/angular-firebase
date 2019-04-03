5a6f441e51135180be72b2b2900550d2b3c98734
Aitor I Bertorelli E <publicidad@aibe.pro>
2019-04-03 22:03:02 +0200

    Firebase hosting deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCCCrnifT9SVBV-zzmXRBbSsRx85JwQeOA",
+    authDomain: "coffeshop-6aef1.firebaseapp.com",
+    databaseURL: "https://coffeshop-6aef1.firebaseio.com",
+    projectId: "coffeshop-6aef1",
+    storageBucket: "coffeshop-6aef1.appspot.com",
+    messagingSenderId: "239832322422"
+  }
 };
